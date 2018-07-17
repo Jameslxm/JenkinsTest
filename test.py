2f8621d7c829e1e3de80eb854e048b674207136b
lixinman <lixinman@126.com>
2018-07-17 14:41:51 +0800

    python

---
+++ test.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+#test_copyfile.py
+import os,shutil
+def mymovefile(srcfile,dstfile):
+    if not os.path.isfile(srcfile):
+        print ("==="+srcfile)
+    else:
+        fpath,fname=os.path.split(dstfile)    #分离文件名和路径
+        if not os.path.exists(fpath):
+            os.makedirs(fpath)                #创建路径
+        shutil.copyfile(srcfile,dstfile)          #移动文件
+        print ("move %s -> %s" ,srcfile,dstfile)
+        
+srcfile='./app/build/outputs/apk/xiaomi/release/AutoBuildTest-v1.0.1-xiaomi-release.apk'
+dstfile='D:/download/AutoBuildTest-v1.0.1-xiaomi-release.apk'
+mymovefile(srcfile,dstfile)
